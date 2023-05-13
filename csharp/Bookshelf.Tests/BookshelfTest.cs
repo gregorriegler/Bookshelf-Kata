a086bc0bc938d96fdb2fa577b4fffb3a6d17aba1
Gregor Riegler <gregor.riegler@ebcont.com>
2023-05-13 13:11:05 +0200

    stores books ad dicts

---
+++ csharp/Bookshelf.Tests/BookshelfTest.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using Bookshelf.Db;
+using Bookshelf.Db2;
+
+namespace Bookshelf.Tests
+{
+    public class BookshelfTest
+    {
+        private DbFactory.Db<Dictionary<string, object>> _db;
+
+        public BookshelfTest()
+        {
+            var dbFactory = DbFactory.GetInstance();
+            _db = dbFactory.startDb<Dictionary<string, object>>();
+        }
+
+        [Fact]
+        public void StartsEmpty()
+        {
+            var bookshelf = new Bookshelf(_db);
+
+            var books = bookshelf.GetAll();
+
+            books.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void RetrievesSingleStoredBook()
+        {
+            var book = new Book("Refactoring");
+            _db.Persist(new Dictionary<string, object>()
+            {
+                {"name", "Refactoring"},
+                {"anniversary", false}
+            });
+            var bookshelf = new Bookshelf(_db);
+
+            var books = bookshelf.GetAll();
+
+            books.Should().ContainSingle().Which.Should().BeEquivalentTo(book);
+        }
+
+        [Fact]
+        public void AddsBook()
+        {
+            var bookshelf = new Bookshelf(_db);
+            var book = new Book("Refactoring");
+
+            bookshelf.Add(book);
+
+            bookshelf.GetAll().Should().ContainSingle().Which.Should().BeEquivalentTo(book);
+        }
+
+        [Fact]
+        public void The5thBookIsAnniversary()
+        {
+            var bookshelf = new Bookshelf(_db);
+            bookshelf.Add(new Book("NotAAnniversary1"));
+            bookshelf.Add(new Book("NotAAnniversary2"));
+            bookshelf.Add(new Book("NotAAnniversary3"));
+            bookshelf.Add(new Book("NotAAnniversary4"));
+            
+            bookshelf.Add(new Book("Anniversary5"));
+
+            bookshelf.GetAll().Should().Contain(
+                new List<Book>
+                {
+                    new("NotAAnniversary1"),
+                    new("NotAAnniversary2"),
+                    new("NotAAnniversary3"),
+                    new("NotAAnniversary4"),
+                    new("Anniversary5", true)
+                }
+            );
+        }
+    }
+}
--- csharp/Bookshelf/Bookshelf.cs
+++ csharp/Bookshelf/Bookshelf.cs
@@ -4,31 +4,78 @@ namespace Bookshelf
 {
     public class Bookshelf
     {
-        private readonly DbFactory.Db<Book> _db;
+        private readonly DbFactory.Db<Dictionary<string,object>> _db;
+        private int _timesAddedABook;
 
-        public Bookshelf(DbFactory.Db<Book> db)
+        public Bookshelf(DbFactory.Db<Dictionary<string,object>> db)
         {
             _db = db;
         }
 
         public List<Book> GetAll()
         {
-            return _db.FindAll();
+            Converter<Dictionary<string,object>,Book> converter = toBook;
+            return _db.FindAll().ConvertAll(converter);
+        }
+
+        private static Book toBook(Dictionary<string, object> dict)
+        {
+            return new Book((string)dict["name"], (bool)dict["anniversary"]);
+        }
+
+        private static Dictionary<string, object> toDict(Book book)
+        {
+            var dictionary = new Dictionary<string, object>
+            {
+                { "name", book.Name },
+                { "anniversary", book.Anniversary }
+            };
+            return dictionary;
         }
 
         public void Add(Book book)
         {
-            _db.Persist(book);
+            if (_timesAddedABook == 4)
+            {
+                book.Anniversary = true;
+            }
+            _db.Persist(toDict(book));
+            _timesAddedABook++;
         }
     }
 
     public class Book
     {
-        public string Refactoring { get; }
+        public string Name { get; }
 
-        public Book(string refactoring)
+        public bool Anniversary { get; set; }
+
+        public Book(string name)
         {
-            Refactoring = refactoring;
+            Name = name;
+        }
+
+        public Book(string name, bool anniversary) : this(name)
+        {
+            Anniversary = anniversary;
+        }
+
+        private bool Equals(Book other)
+        {
+            return Name == other.Name && Anniversary == other.Anniversary;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Book)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Anniversary);
         }
     }
 }
