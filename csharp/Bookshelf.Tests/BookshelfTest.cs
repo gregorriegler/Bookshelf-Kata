e34d174ecb9afab5d8338fcba9ddb6d99a4e5dcd
Gregor Riegler <gregor.riegler@ebcont.com>
2023-05-15 15:22:15 +0200

    renaming

--- csharp/Bookshelf.Tests/BookshelfTest.cs
+++ csharp/Bookshelf.Tests/BookshelfTest.cs
@@ -3,76 +3,75 @@ using FluentAssertions;
 using Xunit;
 using Bookshelf.Db;
 
-namespace Bookshelf.Tests
+namespace Bookshelf.Tests;
+
+public class BookshelfTest
 {
-    public class BookshelfTest
+    private readonly DbFactory.Db<Dictionary<string, object>> _db;
+
+    public BookshelfTest()
     {
-        private DbFactory.Db<Dictionary<string, object>> _db;
+        var dbFactory = DbFactory.GetInstance();
+        _db = dbFactory.startDb<Dictionary<string, object>>();
+    }
 
-        public BookshelfTest()
+    [Fact]
+    public void StartsEmpty()
+    {
+        var bookshelf = new Bookshelf(_db);
+
+        var books = bookshelf.GetAll();
+
+        books.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RetrievesSingleStoredBook()
+    {
+        var book = new Book("Refactoring");
+        _db.Persist(new Dictionary<string, object>
         {
-            var dbFactory = DbFactory.GetInstance();
-            _db = dbFactory.startDb<Dictionary<string, object>>();
-        }
+            {"name", "Refactoring"},
+            {"anniversary", false}
+        });
+        var bookshelf = new Bookshelf(_db);
 
-        [Fact]
-        public void StartsEmpty()
-        {
-            var bookshelf = new Bookshelf(_db);
+        var books = bookshelf.GetAll();
 
-            var books = bookshelf.GetAll();
+        books.Should().ContainSingle().Which.Should().BeEquivalentTo(book);
+    }
 
-            books.Should().BeEmpty();
-        }
+    [Fact]
+    public void AddsBook()
+    {
+        var bookshelf = new Bookshelf(_db);
+        var book = new Book("Refactoring");
 
-        [Fact]
-        public void RetrievesSingleStoredBook()
-        {
-            var book = new Book("Refactoring");
-            _db.Persist(new Dictionary<string, object>
-            {
-                {"name", "Refactoring"},
-                {"anniversary", false}
-            });
-            var bookshelf = new Bookshelf(_db);
+        bookshelf.Add(book);
 
-            var books = bookshelf.GetAll();
+        bookshelf.GetAll().Should().ContainSingle().Which.Should().BeEquivalentTo(book);
+    }
 
-            books.Should().ContainSingle().Which.Should().BeEquivalentTo(book);
-        }
-
-        [Fact]
-        public void AddsBook()
-        {
-            var bookshelf = new Bookshelf(_db);
-            var book = new Book("Refactoring");
-
-            bookshelf.Add(book);
-
-            bookshelf.GetAll().Should().ContainSingle().Which.Should().BeEquivalentTo(book);
-        }
-
-        [Fact]
-        public void The5thBookIsAnniversary()
-        {
-            var bookshelf = new Bookshelf(_db);
-            bookshelf.Add(new Book("NotAAnniversary1"));
-            bookshelf.Add(new Book("NotAAnniversary2"));
-            bookshelf.Add(new Book("NotAAnniversary3"));
-            bookshelf.Add(new Book("NotAAnniversary4"));
+    [Fact]
+    public void The5ThBookIsAnniversary()
+    {
+        var bookshelf = new Bookshelf(_db);
+        bookshelf.Add(new Book("NotAAnniversary1"));
+        bookshelf.Add(new Book( "NotAAnniversary2"));
+        bookshelf.Add(new Book("NotAAnniversary3"));
+        bookshelf.Add(new Book("NotAAnniversary4"));
             
-            bookshelf.Add(new Book("Anniversary5"));
+        bookshelf.Add(new Book("Anniversary5"));
 
-            bookshelf.GetAll().Should().Contain(
-                new List<Book>
-                {
-                    new("NotAAnniversary1"),
-                    new("NotAAnniversary2"),
-                    new("NotAAnniversary3"),
-                    new("NotAAnniversary4"),
-                    new("Anniversary5", true)
-                }
-            );
-        }
+        bookshelf.GetAll().Should().Contain(
+            new List<Book>
+            {
+                new("NotAAnniversary1"),
+                new("NotAAnniversary2"),
+                new("NotAAnniversary3"),
+                new("NotAAnniversary4"),
+                new("Anniversary5", true)
+            }
+        );
     }
 }
--- csharp/Bookshelf/Bookshelf.cs
+++ csharp/Bookshelf/Bookshelf.cs
@@ -1,45 +1,44 @@
 ﻿using Bookshelf.Db;
 
-namespace Bookshelf
+namespace Bookshelf;
+
+public class Bookshelf
 {
-    public class Bookshelf
+    private readonly DbFactory.Db<Dictionary<string,object>> _db;
+    private int _timesAddedABook;
+
+    public Bookshelf(DbFactory.Db<Dictionary<string,object>> db)
     {
-        private readonly DbFactory.Db<Dictionary<string,object>> _db;
-        private int _timesAddedABook;
+        _db = db;
+    }
 
-        public Bookshelf(DbFactory.Db<Dictionary<string,object>> db)
-        {
-            _db = db;
-        }
+    public List<Book> GetAll()
+    {
+        return _db.FindAll().ConvertAll(ToBook);
+    }
 
-        public List<Book> GetAll()
+    public void Add(Book book)
+    {
+        if (_timesAddedABook == 4)
         {
-            return _db.FindAll().ConvertAll(toBook);
+            book.Anniversary = true;
         }
+        _db.Persist(ToDict(book));
+        _timesAddedABook++;
+    }
 
-        public void Add(Book book)
-        {
-            if (_timesAddedABook == 4)
-            {
-                book.Anniversary = true;
-            }
-            _db.Persist(toDict(book));
-            _timesAddedABook++;
-        }
+    private static Book ToBook(Dictionary<string, object> dict)
+    {
+        return new Book((string)dict["name"], (bool)dict["anniversary"]);
+    }
 
-        private static Book toBook(Dictionary<string, object> dict)
+    private static Dictionary<string, object> ToDict(Book book)
+    {
+        var dictionary = new Dictionary<string, object>
         {
-            return new Book((string)dict["name"], (bool)dict["anniversary"]);
-        }
-
-        private static Dictionary<string, object> toDict(Book book)
-        {
-            var dictionary = new Dictionary<string, object>
-            {
-                { "name", book.Name },
-                { "anniversary", book.Anniversary }
-            };
-            return dictionary;
-        }
+            { "name", book.Name },
+            { "anniversary", book.Anniversary }
+        };
+        return dictionary;
     }
 }
